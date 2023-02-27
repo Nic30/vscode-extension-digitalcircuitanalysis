6e32e6f26250b01475d5fad945c0b0375848322e
Matej-V <matej.vadovic@gmail.com>
2023-02-27 19:28:58 +0100

    add hwscheduling findWidget implementation

--- src/media/hwscheduling.ts
+++ src/media/hwscheduling.ts
@@ -3,14 +3,142 @@ import * as d3 from 'd3';
 //const HwSchedulingTimelineGraph = d3.HwSchedulingTimelineGraph;
 import { _vscode } from './vscodePlaceholder';
 export declare const vscode: _vscode;
+import { initializeFindWidget, FindWidgetFormData } from './findWidget';
+import { select } from 'd3';
+import { TimelineItemData } from 'd3-hwschedulinggraphs/dist/data';
+import { HwSchedulingTimelineGraph } from 'd3-hwschedulinggraphs';
+import { deflateRaw } from 'zlib';
+
 
 
 const graphContainer = document.getElementById('timelineGraphContainer');
 const errorContainer = document.createElement('div');
-
+let timeline = (null as any);
 document.body.appendChild(errorContainer);
 errorContainer.className = 'error';
 errorContainer.style.display = 'none';
+
+/**
+ * 
+ * Creates dictionary of id to list of successor ids.
+ * @param nodes To be searched
+ * @return dictionary of id to list of successor ids
+ */
+function getSuccessorDict(nodes: TimelineItemData[]) {
+	const dict: { [id: number]: number[] } = {};
+	for (const item of nodes){
+		dict[item.id] = [];
+	}
+	for (const item of nodes) {
+		for (const port of item.portsIn) {
+			const predecessorID = port[2];
+			const successorList = dict[predecessorID];
+			successorList.push(item.id);
+		}
+	}
+	return dict;
+}
+
+/**
+ * 
+ * Add the selected nodes to the left of startingNode to the selection of the timeline.
+ * @param startingNode 
+ * @param distance 
+ * @param resultSelected 
+ * @param idToDataDict 
+ */
+function addTimelineItemsLeft(startingNode: TimelineItemData, distance: number, resultSelected: Set<TimelineItemData>, idToDataDict: { [id: number]: TimelineItemData }) {
+	resultSelected.add(startingNode);
+	if (distance > 0) {
+		for (const port of startingNode.portsIn) {
+			const predecessorID = port[2];
+			const predecessor = idToDataDict[predecessorID];
+			if (!predecessor) {
+				throw new Error("Predecessor not found" + predecessorID);
+			}
+			addTimelineItemsLeft(predecessor, distance - 1, resultSelected, idToDataDict);
+		}
+	}
+}
+
+/**
+ * 
+ * Add the selected nodes to the right of startingNode to the selection of the timeline.
+ * @param startingNode Starting node to search from
+ * @param distance Distance to search
+ * @param resultSelected Set of selected nodes
+ * @param idToDataDict Dictionary of id to node
+ */
+function addTimelineItemsRight(startingNode: TimelineItemData, distance: number, resultSelected: Set<TimelineItemData>, idToDataDict: { [id: number]: TimelineItemData }, successorDict: { [id: number]: number[] }) {
+	resultSelected.add(startingNode);
+	if (distance > 0) {
+		for (const successorID of successorDict[startingNode.id]) {
+			const successor = idToDataDict[successorID];
+			if (!successor) {
+				throw new Error("Successor not found" + successorID);
+			}
+			addTimelineItemsRight(successor, distance - 1, resultSelected, idToDataDict, successorDict);
+		}
+	}
+}
+
+/**
+ * 
+ * Add the selected items to the selection of the timeline.
+ * @param formDataJSON 
+ * @return 
+ */
+function onAdd(formDataJSON: FindWidgetFormData) {
+	const lines = d3.selectAll(".hwscheduling-timeline-graph path");
+	const bars = d3.selectAll(".hwscheduling-timeline-graph rect");
+	if (formDataJSON.searchValue == "" || timeline == null) { return; }
+	const searchValues = formDataJSON.searchValue.split(",");
+	let successorDict = {};
+	if (formDataJSON.directionRight) { successorDict = getSuccessorDict(timeline.data.data); }
+	if(formDataJSON.idOrName == "Id"){
+		for (const searchValue of searchValues) {
+			const searchValueInt = parseInt(searchValue);
+			for (const item of bars.data()) {
+				if ((item as TimelineItemData).id == searchValueInt) {
+					if (formDataJSON.directionRight) {
+						addTimelineItemsRight(item as TimelineItemData, formDataJSON.distance, timeline.currentlySelected, timeline.idToDataDict, successorDict);
+					}
+					if (formDataJSON.directionLeft) {
+						addTimelineItemsLeft(item as TimelineItemData, formDataJSON.distance, timeline.currentlySelected, timeline.idToDataDict);
+					}
+				}
+			}
+		}
+	}else{
+		for (const searchValue of searchValues) {
+			for (const item of bars.data()) {
+				if ((item as TimelineItemData).label === searchValue) {
+					if (formDataJSON.directionRight) {
+						addTimelineItemsRight(item as TimelineItemData, formDataJSON.distance, timeline.currentlySelected, timeline.idToDataDict, successorDict);
+					}
+					if (formDataJSON.directionLeft) {
+						addTimelineItemsLeft(item as TimelineItemData, formDataJSON.distance, timeline.currentlySelected, timeline.idToDataDict);
+					}
+				}
+			}
+		}
+	}
+	
+	// Highlight the selected items
+	console.log(timeline.currentlySelected);
+	timeline.applyHighlight();
+}
+/**
+ * Clear the selection of the timeline.
+ */
+function onClearSelection() {
+	timeline.currentlySelected.clear();
+	timeline.applyHighlight();
+}
+
+
+
+initializeFindWidget(document, onAdd, onClearSelection);
 
 /**
  * Render the document in the webview.
@@ -33,7 +161,7 @@ function updateContent(text: string) {
 	if (graphContainer !== null) {
 		graphContainer.style.display = '';
 
-		const timeline = new (d3 as any).HwSchedulingTimelineGraph(graphContainer as HTMLDivElement, json);
+		timeline = new (d3 as any).HwSchedulingTimelineGraph(graphContainer as HTMLDivElement, json);
 		timeline.draw();
 	}
 }
